fa0473a336eaee0db1977313c0da7a07a560dda3
William Freudenberger <w.freude@icloud.com>
2025-05-07 10:12:15 +0200

    fix: opencv qr reader

--- rust/qr_reader_pc/src/lib.rs
+++ rust/qr_reader_pc/src/lib.rs
@@ -123,7 +123,7 @@ fn camera_capture(camera: &mut videoio::VideoCapture, window: &str) -> Result<Gr
     let mut image: GrayImage = ImageBuffer::new(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     let mut ocv_gray_image = Mat::default();
 
-    cvt_color(&frame, &mut ocv_gray_image, COLOR_BGR2GRAY, 0)?;
+    cvt_color(&frame, &mut ocv_gray_image, COLOR_BGR2GRAY, 0, opencv::core::AlgorithmHint::ALGO_HINT_DEFAULT)?;
 
     for y in 0..ocv_gray_image.rows() {
         for x in 0..ocv_gray_image.cols() {
