901c39277ad1a42ae724f65feded51dc8a29b208
William Freudenberger <w.freude@icloud.com>
2025-05-09 02:29:26 +0200

    use other opencv coloring

--- rust/qr_reader_pc/src/lib.rs
+++ rust/qr_reader_pc/src/lib.rs
@@ -14,7 +14,7 @@ use qr_reader_phone::process_payload::{process_decoded_payload, InProgress, Read
 
 use opencv::{
     highgui,
-    imgproc::{cvt_color, COLOR_BGR2GRAY},
+    imgproc::{cvt_color_def, COLOR_BGR2GRAY},
     prelude::*,
     videoio,
     videoio::{CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH},
@@ -123,7 +123,7 @@ fn camera_capture(camera: &mut videoio::VideoCapture, window: &str) -> Result<Gr
     let mut image: GrayImage = ImageBuffer::new(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     let mut ocv_gray_image = Mat::default();
 
-    cvt_color(&frame, &mut ocv_gray_image, COLOR_BGR2GRAY, 0, opencv::core::AlgorithmHint::ALGO_HINT_DEFAULT)?;
+    cvt_color_def(&frame, &mut ocv_gray_image, COLOR_BGR2GRAY)?;
 
     for y in 0..ocv_gray_image.rows() {
         for x in 0..ocv_gray_image.cols() {
